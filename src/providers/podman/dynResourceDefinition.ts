98f1c4dd6effd5f1d195e1fe24bd68c15ab959a2
Javier Viola <javier@parity.io>
2022-02-13 10:39:45 -0300

    fix podman mount volumes

--- src/providers/podman/dynResourceDefinition.ts
+++ src/providers/podman/dynResourceDefinition.ts
@@ -246,20 +246,18 @@ function make_transfer_containter(): any {
 async function make_volume_mounts(name: string): Promise<[any, any]> {
   const volume_mounts = [
     { name: "tmp-cfg", mountPath: "/cfg", readOnly: false },
-    { name: "tmp-z", mountPath: "/z", readOnly: false },
+    { name: "tmp-data", mountPath: "/data", readOnly: false },
   ];
 
   const client = getClient();
   const cfgPath = `${client.tmpDir}/${name}/cfg`;
-  const zPath = `${client.tmpDir}/${name}/z`;
   const dataPath = `${client.tmpDir}/${name}/data`;
   await fs.mkdir(cfgPath, { recursive: true });
-  await fs.mkdir(zPath, { recursive: true });
+
   await fs.mkdir(dataPath, { recursive: true });
 
   const devices = [
     { name: "tmp-cfg", hostPath: { type: "Directory", path: cfgPath } },
-    { name: "tmp-z", hostPath: { type: "Directory", path: zPath } },
     { name: "tmp-data", hostPath: { type: "Directory", path: dataPath } },
   ];
 
