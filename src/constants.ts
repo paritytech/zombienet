cc91dbcd652322d5e2f8ea18efa845b54c812556
Andrew Tomaka <atomaka@atomaka.com>
2022-08-17 13:36:44 -0400

    Fix setting the provider via file (#291)

--- src/constants.ts
+++ src/constants.ts
@@ -61,6 +61,7 @@ export const LOKI_URL_FOR_NODE =
   "https://grafana.parity-mgmt.parity.io/explore?orgId=1&left=%5B%22now-3h%22,%22now%22,%22loki.parity-zombienet%22,%7B%22expr%22:%22%7Bpod%3D~%5C%22{{namespace}}%2F{{podName}}%5C%22%7D%22,%22refId%22:%22A%22,%22range%22:true%7D%5D";
 
 export const AVAILABLE_PROVIDERS = ["podman", "kubernetes", "native"];
+export const DEFAULT_PROVIDER = "kubernetes";
 export const DEV_ACCOUNTS = [
   "alice",
   "bob",
