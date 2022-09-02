1449249d9f3bfe04a5b7b7ef84e45b7cd088ff5e
Javier Viola <javier@parity.io>
2022-09-02 08:32:28 -0300

    fmt

--- src/jsapi-helpers/chain-upgrade.ts
+++ src/jsapi-helpers/chain-upgrade.ts
@@ -119,7 +119,9 @@ async function performChainUpgrade(api: ApiPromise, code: string) {
 
   await new Promise<void>(async (resolve, reject) => {
     const unsub = await api.tx.sudo
-      .sudoUncheckedWeight(api.tx.system.setCodeWithoutChecks(`0x${code}`), {refTime: 1})
+      .sudoUncheckedWeight(api.tx.system.setCodeWithoutChecks(`0x${code}`), {
+        refTime: 1,
+      })
       .signAndSend(alice, (result) => {
         console.log(`Current status is ${result.status}`);
         if (result.status.isInBlock) {
