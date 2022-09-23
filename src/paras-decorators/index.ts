e22ad64026a33d48046e1f9f3fd0aa78cd9721c8
Javier Viola <javier@parity.io>
2022-09-23 07:01:47 -0300

    fmt

--- src/paras-decorators/index.ts
+++ src/paras-decorators/index.ts
@@ -1,52 +1,53 @@
-
 enum PARA {
-    Statemint = "statemint",
-    Moonbeam = "moonbeam",
-    Generic = "generic"
-   }
+  Statemint = "statemint",
+  Moonbeam = "moonbeam",
+  Generic = "generic",
+}
 
-interface ParaDecorator { [fn: string] : Function };
+interface ParaDecorator {
+  [fn: string]: Function;
+}
 
 // imports
 import moonbeam from "./moonbeam";
 import statemint from "./statemint";
 
-
 function whichPara(chain: string): PARA {
-    if( chain.includes("statemint")) return PARA.Statemint;
-    if( chain.match(/["moonbase"|"moonriver"|"moonbeam"]/ig)) return PARA.Moonbeam;
+  if (chain.includes("statemint")) return PARA.Statemint;
+  if (chain.match(/["moonbase"|"moonriver"|"moonbeam"]/gi))
+    return PARA.Moonbeam;
 
-    return PARA.Generic;
+  return PARA.Generic;
 }
 
-
-const moonbeamDecorators: ParaDecorator = Object.keys(moonbeam).reduce((memo, fn) => {
+const moonbeamDecorators: ParaDecorator = Object.keys(moonbeam).reduce(
+  (memo, fn) => {
     memo[fn] = (moonbeam as ParaDecorator)[fn];
     return memo;
-},Object.create({}));
+  },
+  Object.create({}),
+);
 
-const statemintDecorators: ParaDecorator = Object.keys(statemint).reduce((memo, fn) => {
+const statemintDecorators: ParaDecorator = Object.keys(statemint).reduce(
+  (memo, fn) => {
     memo[fn] = (statemint as ParaDecorator)[fn];
     return memo;
-},Object.create({}));
+  },
+  Object.create({}),
+);
 
-const decorators : {[para in PARA]: { [fn: string]: Function}} = {
-    moonbeam : moonbeamDecorators,
-    statemint: statemintDecorators,
-    generic: {}
+const decorators: { [para in PARA]: { [fn: string]: Function } } = {
+  moonbeam: moonbeamDecorators,
+  statemint: statemintDecorators,
+  generic: {},
 };
 
-function decorate(para: PARA , fns: Function[]) {
-    const decorated = fns.map( fn => {
-        return (decorators[para][fn.name]) ? decorators[para][fn.name] : fn;
-    });
+function decorate(para: PARA, fns: Function[]) {
+  const decorated = fns.map((fn) => {
+    return decorators[para][fn.name] ? decorators[para][fn.name] : fn;
+  });
 
-    return decorated;
+  return decorated;
 }
 
-export {
-    whichPara,
-    decorate,
-    PARA,
-  };
-
+export { whichPara, decorate, PARA };
