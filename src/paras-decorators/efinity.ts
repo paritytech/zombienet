8690f445fff9f13251aa68c9c16cd4c9084d8fa5
Dastanbek Samatov <dastanbeksamatov@gmail.com>
2022-09-26 18:58:20 +0300

    Add efinity decorators

---
+++ src/paras-decorators/efinity.ts
@@ -0,0 +1,29 @@
+import { Node } from "../types";
+
+// Efinity genesis node key type
+export type GenesisNodeKey = [string, string, { [key: string]: string }];
+
+export function getNodeKey(
+  node: Node,
+  useStash: boolean = true,
+): GenesisNodeKey {
+  console.log("We are using this", node);
+  const { sr_stash, sr_account, ed_account, ec_account } = node.accounts;
+
+  const address = useStash ? sr_stash.address : sr_account.address;
+
+  const key: GenesisNodeKey = [
+    address,
+    address,
+    {
+      aura: address,
+      pools: address,
+    },
+  ];
+
+  return key;
+}
+
+export default {
+  getNodeKey,
+};
--- src/paras-decorators/index.ts
+++ src/paras-decorators/index.ts
@@ -1,6 +1,7 @@
 enum PARA {
   Statemint = "statemint",
   Moonbeam = "moonbeam",
+  Efinity = "efinity",
   Generic = "generic",
 }
 
@@ -11,11 +12,12 @@ interface ParaDecorator {
 // imports
 import moonbeam from "./moonbeam";
 import statemint from "./statemint";
+import efinity from "./efinity";
 
 function whichPara(chain: string): PARA {
   if (chain.includes("statemint")) return PARA.Statemint;
-  if (chain.match(/["moonbase"|"moonriver"|"moonbeam"]/gi))
-    return PARA.Moonbeam;
+  if (/moonbase|moonriver|moonbeam/.test(chain)) return PARA.Moonbeam;
+  if (/efinity|rocfinity/.test(chain)) return PARA.Efinity;
 
   return PARA.Generic;
 }
@@ -36,9 +38,18 @@ const statemintDecorators: ParaDecorator = Object.keys(statemint).reduce(
   Object.create({}),
 );
 
+const efinityDecorators: ParaDecorator = Object.keys(efinity).reduce(
+  (memo, fn) => {
+    memo[fn] = (efinity as ParaDecorator)[fn];
+    return memo;
+  },
+  Object.create({}),
+);
+
 const decorators: { [para in PARA]: { [fn: string]: Function } } = {
   moonbeam: moonbeamDecorators,
   statemint: statemintDecorators,
+  efinity: efinityDecorators,
   generic: {},
 };
 
