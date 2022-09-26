c76aa51ec819e58a5abaa676ccf8b5cb1ecdcb0c
Javier Viola <javier@parity.io>
2022-09-26 07:57:50 -0300

    Add wasm_bindgen to expose parse_to_json fn

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -6,6 +6,8 @@ mod errors;
 
 use ast::{Assertion, AssertionKind, Comparison, TestDefinition};
 
+use wasm_bindgen::prelude::*;
+
 #[cfg(test)]
 mod tests;
 
@@ -499,4 +501,13 @@ pub fn parse(unparsed_file: &str) -> Result<ast::TestDefinition, errors::ParserE
     };
 
     return Ok(test_def);
+}
+
+
+#[wasm_bindgen]
+pub fn parse_to_json(unparsed_file: &str) -> Result<String,String> {
+    if unparsed_file == "" { return Err("error".to_string())}
+    let ast = parse(&unparsed_file).map_err(|e| { e.to_string()})?;
+    let ast_json = serde_json::to_string_pretty(&ast).map_err(|_| {"Serializing error".to_string()})?;
+    Ok(ast_json)
 }
