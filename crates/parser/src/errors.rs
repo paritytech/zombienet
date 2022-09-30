62c0950d6b4b27aa4c250a48860dccdadb834323
Javier Viola <javier@parity.io>
2022-09-30 06:48:47 -0300

    Update crates/parser/src/errors.rs
    Co-authored-by: Nikos Kontakis <wirednkod@gmail.com>

--- crates/parser/src/errors.rs
+++ crates/parser/src/errors.rs
@@ -3,7 +3,7 @@
 pub enum ParserError {
     #[error("Error parsing file: {0}")]
     ParseError(String),
-    #[error("Invalid matching Rule. \n {0}")]
+    #[error("Invalid matching rule. \n {0}")]
     InvalidRule(String),
     #[error("Missing fields: {0}")]
     MissingFields(String),
