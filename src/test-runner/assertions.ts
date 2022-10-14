819149d8dffe890e75b98c64e3b717c526e5d99f
Javier Viola <javier@parity.io>
2022-10-14 10:04:02 -0300

    fix

--- src/test-runner/assertions.ts
+++ src/test-runner/assertions.ts
@@ -178,9 +178,8 @@ const CustomJs = ({
         return { name, wsUri, prometheusUri, userDefinedTypes };
       }),
       paras: Object.keys(network.paras).reduce((memo: any, paraId: any) => {
-        const { chainSpecPath, wasmPath, statePath } = network.paras[paraId]
-        memo[paraId] = { chainSpecPath, wasmPath, statePath }
-        };
+        const { chainSpecPath, wasmPath, statePath } = network.paras[paraId];
+        memo[paraId] = { chainSpecPath, wasmPath, statePath };
         memo[paraId].nodes = network.paras[paraId].nodes.map((node) => {
           return { ...node };
         });
