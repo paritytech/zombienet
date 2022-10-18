4ba06b346fff6ff89cfc667ef92c8deca6fdc88f
wirednkod <wirednkod@gmail.com>
2022-10-18 20:17:53 +0300

    Move test reporter in test-runner dir

--- javascript/packages/orchestrator/src/test-runner/index.ts
+++ javascript/packages/orchestrator/src/test-runner/index.ts
@@ -226,10 +226,7 @@ export async function run(
   }
 
   // pass the file path, don't load the reporter as a module
-  const resolvedReporterPath = path.resolve(
-    __dirname,
-    "../../node_modules/@zombienet/utils/dist/testReporter",
-  );
+  const resolvedReporterPath = path.resolve(__dirname, "./testReporter");
   mocha.reporter(resolvedReporterPath);
 
   // run
