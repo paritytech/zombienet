9f51226de8de0bc6014e9ccc95fbf8687de6aedd
Fahrrader <fhakimov@usetech.com>
2023-04-19 07:55:17 +0000

    test(chain spec mutator): test components + add to CI

---
+++ tests/check-testnet-name.js
@@ -0,0 +1,12 @@
+// Check if the testnet's name is the same as the expected name from the args
+async function run(nodeName, networkInfo, args) {
+    const {wsUri, userDefinedTypes} = networkInfo.nodesByName[nodeName];
+    const api = await zombie.connect(wsUri, userDefinedTypes);
+
+    const expectedName = args[0] ?? "Rococo Local Testnet";
+    const testnetName = await api.rpc.system.chain();
+
+    return testnetName == expectedName;
+}
+
+module.exports = { run }
