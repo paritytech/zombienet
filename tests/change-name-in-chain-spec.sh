f830ed349493697cb8f6900514de9c57e48f31ed
Fahrrader <fhakimov@usetech.com>
2023-04-20 10:44:51 +0000

    test(chain spec mutator): fix faulty mutator script

--- tests/change-name-in-chain-spec.sh
+++ tests/change-name-in-chain-spec.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # The first argument sent to the script must be the path of the chain spec
-chain_spec_path = $1
+chain_spec_path=$1
 # Assume that the name 
 old_name="Rococo Local Testnet"
 # Set the new name for the network
 new_name="Tentset Lacol Ococor"
 
+input=$(cat $chain_spec_path)
+
 # Replace the name field in the JSON file
-sed -i "s/\"name\": \"$old_name\"/\"name\": \"$new_name\"/" $chain_spec_path
+echo "$input" | sed "s/\"name\": \"$old_name\"/\"name\": \"$new_name\"/"
