5f23ccf021ad98cb7506591f0ab083b4f108995e
Nikos Kontakis <wirednkod@gmail.com>
2023-05-10 21:55:51 +0300

    Add parser-wrapper doc (#1034)

--- javascript/packages/cli/src/actions/test.ts
+++ javascript/packages/cli/src/actions/test.ts
@@ -8,7 +8,7 @@ import path from "path";
 import { AVAILABLE_PROVIDERS } from "../constants";
 
 /**
- * Test - performs test/assertions agins the spawned network, using a set of natural
+ * Test - performs test/assertions against the spawned network, using a set of natural
  * language expressions that allow to make assertions based on metrics, logs and some
  * built-in function that query the network using polkadot.js
  * Read more here: https://paritytech.github.io/zombienet/cli/testing.html
