16c9f15511d4b0861c233b4a13e02d62f6db7a71
Javier Viola <javier@parity.io>
2023-08-17 10:18:01 -0300

    change script for testing feature

--- examples/check_creations.sh
+++ examples/check_creations.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-path=`pwd`
-some=${path##*/}
-cd keystore
 
-if [ $some = "alice" ];
+KEY_ENCODED=$1
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+if [[ $SCRIPTPATH == "/tmp" ]]; then
+    cd ../data
+else
+    # native provider use `root dir` to run the script
+    cd data
+fi
+
+if [ $KEY_ENCODED = "ALL" ];
 then
-    ## alice has all key types (11) thus we just count the number of files in keystore dir
+    ## should have all key types (11) thus we just count the number of files in keystore dir
+    cd $(find . -name keystore)
     count=$(ls | wc -l)
-    if [ $count = 11 ]; then
+    if [ $count -eq 11 ]; then
         exit 0
     fi
-elif [ $some = "bob" ];
-then
-    result=`ls $(echo -n aura | hexdump -v -e '/1 "%02X"')*`
-    count=$(echo -e "$result" | wc -l)
-    if [ $count = 1 ]; then
+else
+    count=$(find . -name "${KEY_ENCODED}*" |wc -l)
+    if [ $count -eq 1 ]; then
         exit 0
     fi
-elif [ $some = "collator01" ];
-then
-    result=`ls $(echo -n gran | hexdump -v -e '/1 "%02X"')*`
-    count=$(echo -e "$result" | wc -l)
-    exit 0
 fi
 
 echo "ERRORED"
