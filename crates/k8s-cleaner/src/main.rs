772a32cc4764ab8915fec5c057b9e3623dfee408
Javier Viola <pepoviola@gmail.com>
2023-08-30 12:12:27 -0300

    add cleaner util

---
+++ crates/k8s-cleaner/src/main.rs
@@ -0,0 +1,139 @@
+use k8s_openapi::api::core::v1::Namespace;
+use kube::{
+    api::{Api, DeleteParams, ListParams},
+    Client,
+};
+
+use futures::stream::FuturesUnordered;
+
+#[derive(Debug, Clone)]
+pub struct NamespaceCI {
+    ns_name: String,
+    job_id: String,
+}
+
+impl NamespaceCI {
+    pub fn new(ns_name: impl Into<String>, job_id: impl Into<String>) -> Self {
+        Self {
+            ns_name: ns_name.into(),
+            job_id: job_id.into(),
+        }
+    }
+}
+
+const CI_URL: &str = "https://gitlab.parity.io/parity/mirrors/polkadot-sdk/-/jobs";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::try_default().await?;
+
+    let namespaces: Api<Namespace> = Api::<Namespace>::all(client);
+    let list_params = ListParams::default();
+    let nss = namespaces.list(&list_params).await?;
+    let list = nss.iter().filter_map(|ns| {
+        if let Some(name) = ns.metadata.name.as_ref() {
+            if !name.contains("zombie-") {
+                return None;
+            } else {
+                let job_id = match get_job_id_from_annotations(&ns) {
+                    Ok(job_id) => job_id,
+                    Err(_) => { return None }
+                };
+
+                let ns = NamespaceCI::new(
+                    name.as_str(),
+                    job_id
+                );
+                return Some(ns);
+            }
+        } else {
+            return None;
+        }
+    });
+
+    let req = reqwest::Client::new();
+    let tasks = list
+        .map(|ns| {
+            let r = req.clone();
+            tokio::spawn(async move {
+                if let Ok(needs) = needs_to_delete(r, ns.job_id.as_str()).await {
+                    if needs {
+                        Some(ns.ns_name.to_owned())
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+        })
+        .collect::<FuturesUnordered<_>>();
+
+    let result = futures::future::join_all(tasks).await;
+
+    //delete
+    let delete_task: Vec<_> = result
+        .into_iter()
+        .map(|r| {
+            if let Ok(val) = r {
+                if let Some(name) = val {
+                    Some(name)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .flatten()
+        .collect();
+
+    let t = delete_task
+        .into_iter()
+        .map(|name| {
+            let ns = namespaces.clone();
+            tokio::spawn(async move {
+                println!("deleting ns: {}", name);
+                ns.delete(name.clone().as_str(), &DeleteParams::default())
+                    .await
+            })
+        })
+        .collect::<FuturesUnordered<_>>();
+
+    let _result = futures::future::join_all(t).await;
+
+    Ok(())
+}
+
+
+fn get_job_id_from_annotations(ns: &Namespace) -> Result<String, Box<dyn std::error::Error>> {
+    let annotation: serde_json::Value = serde_json::from_str(
+        ns.metadata
+            .annotations
+            .as_ref()
+            .ok_or("annotation not found")?
+            .get("kubectl.kubernetes.io/last-applied-configuration")
+            .ok_or("value not found")?
+    )?;
+
+    let job_id = annotation["metadata"]["labels"]["jobId"]
+                        .as_str().ok_or("can't find jobId")?;
+    Ok(job_id.into())
+}
+async fn needs_to_delete(
+    req: reqwest::Client,
+    job_id: &str,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    let res = req
+        .get(format!("{}/{}", CI_URL, job_id))
+        .header("Accept", "application/json")
+        .send()
+        .await?
+        .text()
+        .await?;
+    let res_json: serde_json::Value = serde_json::from_str(res.as_str())?;
+    let status = res_json["status"]["text"].as_str().unwrap_or_default();
+    let needs = status.contains("failed") || status.contains("canceled");
+    println!("jobid: {job_id} - status: {status} - will delete: {needs}");
+    Ok(needs)
+}
