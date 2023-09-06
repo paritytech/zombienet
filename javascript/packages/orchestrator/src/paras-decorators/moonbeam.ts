d345938c58378437378480a07c8ebd822d747389
Javier Viola <javier@parity.io>
2023-09-06 10:04:58 -0300

    fix dup balances in moonbeam chain-spec (#1329)
    * fix dup balances in moonbeam chain-spec
    
    * fmt

--- javascript/packages/orchestrator/src/paras-decorators/moonbeam.ts
+++ javascript/packages/orchestrator/src/paras-decorators/moonbeam.ts
@@ -135,10 +135,27 @@ async function addParaCustom(specPath: string, node: Node) {
   const reservedBalance = BigInt("100000000000000000000");
 
   // Ensure collator account has enough balance to bond and add candidate
+  const node_index = runtimeConfig.balances.balances.findIndex(
+    (item: [string, BigInt]) => {
+      return item[0] === eth_account.address.toLowerCase();
+    },
+  );
+
+  if (node_index > -1) runtimeConfig.balances.balances.splice(node_index, 1);
+
   runtimeConfig.balances.balances.push([
     eth_account.address,
     stakingBond + reservedBalance,
   ]);
+
+  const candidate_index = runtimeConfig.balances.balances.findIndex(
+    (item: [string, BigInt]) => {
+      return item[0] === eth_account.address.toLowerCase();
+    },
+  );
+
+  if (candidate_index > -1)
+    runtimeConfig.balances.balances.splice(candidate_index, 1);
 
   runtimeConfig.parachainStaking.candidates.push([
     eth_account.address,
