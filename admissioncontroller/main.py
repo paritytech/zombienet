a3972dde8aa3616c1e54e27907641d3ac74827fa
Hamid Emamian <emami.he@gmail.com>
2023-10-17 13:44:42 +0300

    bugfix for metadata key

--- admissioncontroller/main.py
+++ admissioncontroller/main.py
@@ -12,7 +12,7 @@ def deployment_webhook_mutate():
     request_info = request.get_json()
     namespace=request_info["request"]["namespace"]
     hasNodeSelector="nodeSelector" in request_info["request"]["object"]["spec"].keys()
-    labels=request_info["request"]["object"]["labels"] if "labels" in request_info["request"]["object"].keys() else {}
+    labels=request_info["request"]["object"]["metadata"]["labels"] if "labels" in request_info["request"]["object"]["metadata"].keys() else {}
     if namespace.startswith("zombie-") and "x-infra-instance" in labels.keys():
        if labels["x-infra-instance"]=="ondemand":
           return admission_response_patch(True, "Adding allow label", json_patch = jsonpatch.JsonPatch([{"op": "add", "path": "/spec/tolerations", "value": [{"effect":"NoExecute", "key":"workload-type", 
