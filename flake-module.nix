d0637fc14ee549a1060a92da0f8bc325d344553c
Javier Viola <javier@parity.io>
2023-12-04 19:20:46 -0300

    nix hash

--- flake-module.nix
+++ flake-module.nix
@@ -8,7 +8,7 @@
     ...
   }: let
     # this change on each change of dependencies, unfortunately this hash not yet automatically updated from SRI of package.lock
-    npmDepsHash = "sha256-IwopIBU33FGeyzM9gGFf+fCBajC73xNzUj98Mjb3ANI=";
+    npmDepsHash = "sha256-DI22Ywb0ZWIZAnfjqpkGbA0T0g3MbsvdtoI1OeeXLjQ=";
     ####
 
     # there is officia polkadot on nixpkgs, but it has no local rococo wasm to run
