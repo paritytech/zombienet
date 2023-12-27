5eadc044b05c049e7b14aeb62dadeb20efeeaa2d
Javier Viola <javier@parity.io>
2023-12-27 09:46:56 -0300

    chore: bump versions for release (#1624)
    * chore: bump versions for release
    
    * Fix nix hash
    
    ---------
    
    Co-authored-by: pepoviola <pepoviola@users.noreply.github.com>

--- flake-module.nix
+++ flake-module.nix
@@ -8,7 +8,7 @@
     ...
   }: let
     # this change on each change of dependencies, unfortunately this hash not yet automatically updated from SRI of package.lock
-    npmDepsHash = "sha256-ruo1UWlwlKxhfh7/PWgYSakFhe8Q8Q+9bg7uPDqnLNI=";
+    npmDepsHash = "sha256-PK1YPfUkLWwzpAQ00S7j5VbfLA8G+zxHaDN4Bjwz7lc=";
     ####
 
     # there is officia polkadot on nixpkgs, but it has no local rococo wasm to run
