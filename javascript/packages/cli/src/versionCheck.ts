db3510488799def195f49d3909c40b94710db095
forgetso <forgetso86@gmail.com>
2024-01-30 19:03:09 +0100

    lint

--- javascript/packages/cli/src/versionCheck.ts
+++ javascript/packages/cli/src/versionCheck.ts
@@ -3,11 +3,12 @@ import { decorators } from "@zombienet/utils";
 export const checkNodeVersion = () => {
   const nodeVersion = process.versions.node;
   const requiredNodeVersion = getPackageNodeVersion();
-  if (parseInt(nodeVersion.split(".")[0]) < parseInt(requiredNodeVersion.split(".")[0]) ) {
+  if (
+    parseInt(nodeVersion.split(".")[0]) <
+    parseInt(requiredNodeVersion.split(".")[0])
+  ) {
     console.error(
-      `\n${decorators.red(
-        "Error: ",
-      )} \t ${decorators.bright(
+      `\n${decorators.red("Error: ")} \t ${decorators.bright(
         `Node version ${nodeVersion} is not supported. Please update to Node ${requiredNodeVersion} or above.`,
       )}\n`,
     );
@@ -16,6 +17,8 @@ export const checkNodeVersion = () => {
 };
 
 const getPackageNodeVersion = () => {
-  const { engines: {node}  } = require("../package.json");
+  const {
+    engines: { node },
+  } = require("../package.json");
   return node.replace(/>=\s*/, "");
-}
+};
