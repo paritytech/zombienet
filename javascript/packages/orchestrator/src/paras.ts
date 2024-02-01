4678e9387ffeafbbdfe0fdd2bf60b33f2097d550
Javier Viola <363911+pepoviola@users.noreply.github.com>
2024-02-01 11:08:52 -0300

    fix(orchestrator): Don't use tmp dir in adder/undying collators

--- javascript/packages/orchestrator/src/paras.ts
+++ javascript/packages/orchestrator/src/paras.ts
@@ -242,9 +242,10 @@ export async function generateParachainFiles(
         );
       }
 
-      if (client.providerName === "native") {
-        // inject a tmp base-path to prevent the use of a pre-existing un-purged data directory.
-        // see https://github.com/paritytech/zombienet/issues/1519
+      if (client.providerName === "native" && !parachain.cumulusBased) {
+        // Inject a tmp base-path to prevent the use of a pre-existing un-purged data directory. This should only
+        // be injected for `cumulus` base parachains.
+        // See https://github.com/paritytech/zombienet/issues/1519
         // NOTE: this is only needed in native provider since un k8s/podman the fs is always fresh
         const exportGenesisStateCustomPath = `${client.tmpDir}/export-genesis-state/${parachain.id}`;
         await fs.promises.mkdir(exportGenesisStateCustomPath, {
