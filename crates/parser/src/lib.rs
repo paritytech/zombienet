549ae9607c3d526ecb253044772b66d72a9e214b
Javier Viola <javier@parity.io>
2024-10-11 12:51:16 +0200

    fmt

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -192,7 +192,7 @@ fn parse_custom_script_rule(
         match inner_record.as_rule() {
             Rule::double_quoted_string => {
                 args = Some(inner_record.as_str().trim_matches('"').to_owned());
-            },
+            }
             Rule::single_quoted_string => {
                 args = Some(inner_record.as_str().trim_matches('\'').to_owned());
             }
