fb37dcca5f3642896b3324447c0da890d8ecc651
pepoviola <pepoviola@users.noreply.github.com>
2024-10-21 09:38:24 +0000

    Fix nix hash

--- flake-module.nix
+++ flake-module.nix
@@ -8,7 +8,7 @@
     ...
   }: let
     # this change on each change of dependencies, unfortunately this hash not yet automatically updated from SRI of package.lock
-    npmDepsHash = "sha256-t0DfM7tuSUJX34yDeTe43tjgA/+Pe9qIOynRItFX+F8=";
+    npmDepsHash = "sha256-UOX/9DrmX8dROI2mKCG8QP1KIxMNOimKAVyRxWMlYJ4=";
     ####
 
     # there is officia polkadot on nixpkgs, but it has no local rococo wasm to run
