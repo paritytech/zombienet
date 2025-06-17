paritytech/zombienet

commit 00b5bde907171d1c82c1e9b89754b450885d004a

bind localhost on native (#2020)

javascript/packages/orchestrator/src/cmdGenerator.ts
@@ -69,6 +69,9 @@ export async function genCumulusCollatorCmd(
     "--prometheus-port": true,
   };
 
+  // bind localhost only in native provider
+  const ip_to_bind = useWrapper ? "0.0.0.0" : "127.0.01";
+
   let fullCmd: string[] = [
     nodeSetup.command || DEFAULT_COMMAND,
     "--name",
@@ -80,7 +83,7 @@ export async function genCumulusCollatorCmd(
     "--base-path",
     dataPath,
     "--listen-addr",
-    `/ip4/0.0.0.0/tcp/${nodeSetup.p2pPort ? nodeSetup.p2pPort : P2P_PORT}/ws`,
+    `/ip4/${ip_to_bind}/tcp/${nodeSetup.p2pPort ? nodeSetup.p2pPort : P2P_PORT}/ws`,
     "--prometheus-external",
     "--rpc-cors all",
     // Do not add `--unsafe-rpc-external` in native provider
@@ -302,7 +305,11 @@ export async function genCmd(
     args[listenIndex + 1] = listenAddr;
   } else {
     // no --listen-add args
-    args.push(...["--listen-addr", `/ip4/0.0.0.0/tcp/${nodeSetup.p2pPort}/ws`]);
+    // bind localhost only in native provider
+    const ip_to_bind = useWrapper ? "0.0.0.0" : "127.0.01";
+    args.push(
+      ...["--listen-addr", `/ip4/${ip_to_bind}/tcp/${nodeSetup.p2pPort}/ws`],
+    );
   }
 
   // set our base path

javascript/packages/orchestrator/src/configGenerator.ts
@@ -481,6 +481,12 @@ export async function generateBootnodeSpec(
   const ports = await getPorts(provider, {});
   const externalPorts = await getExternalPorts(provider, ports, {});
 
+  // In native provider bind only localhost and not use --rpc-external
+  const args =
+    provider != "native"
+      ? ["--listen-addr", "/ip4/0.0.0.0/tcp/30333/ws", "--rpc-external"]
+      : ["--listen-addr", "/ip4/127.0.0.1/tcp/30333/ws"];
+
   const nodeSetup: Node = {
     name: "bootnode",
     key: "0000000000000000000000000000000000000000000000000000000000000001",
@@ -489,7 +495,7 @@ export async function generateBootnodeSpec(
     chain: config.relaychain.chain,
     validator: false,
     invulnerable: false,
-    args: ["--listen-addr", "/ip4/0.0.0.0/tcp/30333/ws"],
+    args,
     env: [],
     bootnodes: [],
     telemetryUrl: "",
@@ -500,8 +506,6 @@ export async function generateBootnodeSpec(
     ...ports,
     externalPorts,
   };
-
-  if (provider != "native") nodeSetup.args.push("--rpc-external");
 
   return nodeSetup;
 }

commit 3706ed6d1111df1245439be6e698d9f9505f0d51

Fixed regression while binding localhost on native
Fixed the regression introduced by [#2020](https://github.com/paritytech/zombienet/pull/2020)
where the invalid local address `127.0.01` was used.

javascript/packages/orchestrator/src/cmdGenerator.ts
@@ -70,7 +70,7 @@ export async function genCumulusCollatorCmd(
   };
 
   // bind localhost only in native provider
-  const ip_to_bind = useWrapper ? "0.0.0.0" : "127.0.01";
+  const ip_to_bind = useWrapper ? "0.0.0.0" : "127.0.0.1";
 
   let fullCmd: string[] = [
     nodeSetup.command || DEFAULT_COMMAND,
@@ -306,7 +306,7 @@ export async function genCmd(
   } else {
     // no --listen-add args
     // bind localhost only in native provider
-    const ip_to_bind = useWrapper ? "0.0.0.0" : "127.0.01";
+    const ip_to_bind = useWrapper ? "0.0.0.0" : "127.0.0.1";
     args.push(
       ...["--listen-addr", `/ip4/${ip_to_bind}/tcp/${nodeSetup.p2pPort}/ws`],
     );